345893db9996c443612ce97f162999bb50f7f42a
DorineH <dorinematildehenry@gmail.com>
2025-08-24 20:03:54 +0200

    congif auth

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// import * as nodeCrypto from 'node:crypto';
+
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
@@ -5,14 +7,31 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "http://192.168.1.99:3000",
+    ], // en prod https://then.app
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle("My API")
-    .setDescription("La documentation de mon API NestJS avec Swagger pour mon projet Then")
+    .setDescription(
+      "La documentation de mon API NestJS avec Swagger pour mon projet Then",
+    )
     .setVersion("1.0")
-    .addTag("users")
+    // .addTag("users")
+    .addBearerAuth(
+      { type: "http", scheme: "bearer", bearerFormat: "JWT" },
+      "bearerAuth",
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, "0.0.0.0");
 }
 bootstrap();
